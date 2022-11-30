da0c975ccc6f0c058644bd2cc3965c104e1d418e
11amanraj <110328977+11amanraj@users.noreply.github.com>
2022-11-30 13:44:15 +0530

    4.4: helper functions and unit tests, step2

--- tests/list.test.js
+++ tests/list.test.js
@@ -6,3 +6,65 @@ test('dummy return one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const oneList = [
+    {
+      'title': 'Way of Kings',
+      'author': 'Brandon Sanderson',
+      'url': 'Whatever',
+      'likes': 20000,
+      'id': '6385c6b018f22df7e5e8cc54'
+    }
+  ]
+   
+  test('if list contains only one blog, equals the likes of that blog', () => {
+    const result = listHelper.totalLikes(oneList)
+    expect(result).toBe(20000)
+  })
+})
+
+// const list = [
+//   {
+//     'title': 'Way of Kings',
+//     'author': 'Brandon Sanderson',
+//     'url': 'Whatever',
+//     'likes': 20000,
+//     'id': '6385c6b018f22df7e5e8cc54'
+//   },
+//   {
+//     'title': 'The Hobbit',
+//     'author': 'JRR Tolkien',
+//     'url': 'www.google.com',
+//     'likes': 30000,
+//     'id': '6385c6d618f22df7e5e8cc57'
+//   },
+//   {
+//     'title': 'Deadhouse Gates',
+//     'author': 'Steven Erickson',
+//     'url': 'www.malazan.com',
+//     'likes': 15000,
+//     'id': '6385c87c7a1339f0e967c80c'
+//   },
+//   {
+//     'title': 'Horus Heresy',
+//     'author': 'Dan Abnett',
+//     'url': 'www.warhammer.com',
+//     'likes': 5000,
+//     'id': '6385cccbed1b7fbf96862999'
+//   },
+//   {
+//     'title': 'Dead Rites',
+//     'author': 'Jim Butcher',
+//     'url': 'www.dresdenfiles.com',
+//     'likes': 5000,
+//     'id': '6385d08cb373f7f178bff582'
+//   },
+//   {
+//     'title': 'Game of Thrones',
+//     'author': 'GRR Martin',
+//     'url': 'www.asongoficeandfire.com',
+//     'likes': 25000,
+//     'id': '6385d2e2cfab45040e8e19b7'
+//   }
+// ]
