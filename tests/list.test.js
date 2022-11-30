48939d1327c957e12bed9961d2f20f3e758e581b
11amanraj <110328977+11amanraj@users.noreply.github.com>
2022-11-30 15:04:26 +0530

    4.5*: helper functions and unit tests, step3

--- tests/list.test.js
+++ tests/list.test.js
@@ -21,50 +21,67 @@ describe('total likes', () => {
   test('if list contains only one blog, equals the likes of that blog', () => {
     const result = listHelper.totalLikes(oneList)
     expect(result).toBe(20000)
-  })
+  }) 
 })
 
-// const list = [
-//   {
-//     'title': 'Way of Kings',
-//     'author': 'Brandon Sanderson',
-//     'url': 'Whatever',
-//     'likes': 20000,
-//     'id': '6385c6b018f22df7e5e8cc54'
-//   },
-//   {
-//     'title': 'The Hobbit',
-//     'author': 'JRR Tolkien',
-//     'url': 'www.google.com',
-//     'likes': 30000,
-//     'id': '6385c6d618f22df7e5e8cc57'
-//   },
-//   {
-//     'title': 'Deadhouse Gates',
-//     'author': 'Steven Erickson',
-//     'url': 'www.malazan.com',
-//     'likes': 15000,
-//     'id': '6385c87c7a1339f0e967c80c'
-//   },
-//   {
-//     'title': 'Horus Heresy',
-//     'author': 'Dan Abnett',
-//     'url': 'www.warhammer.com',
-//     'likes': 5000,
-//     'id': '6385cccbed1b7fbf96862999'
-//   },
-//   {
-//     'title': 'Dead Rites',
-//     'author': 'Jim Butcher',
-//     'url': 'www.dresdenfiles.com',
-//     'likes': 5000,
-//     'id': '6385d08cb373f7f178bff582'
-//   },
-//   {
-//     'title': 'Game of Thrones',
-//     'author': 'GRR Martin',
-//     'url': 'www.asongoficeandfire.com',
-//     'likes': 25000,
-//     'id': '6385d2e2cfab45040e8e19b7'
-//   }
-// ]
+describe('favoriteBlogs', () => {
+  const list = [
+    {
+      'title': 'Way of Kings',
+      'author': 'Brandon Sanderson',
+      'url': 'Whatever',
+      'likes': 20000,
+      'id': '6385c6b018f22df7e5e8cc54'
+    },
+    {
+      'title': 'The Hobbit',
+      'author': 'JRR Tolkien',
+      'url': 'www.google.com',
+      'likes': 30000,
+      'id': '6385c6d618f22df7e5e8cc57'
+    },
+    {
+      'title': 'Deadhouse Gates',
+      'author': 'Steven Erickson',
+      'url': 'www.malazan.com',
+      'likes': 15000,
+      'id': '6385c87c7a1339f0e967c80c'
+    },
+    {
+      'title': 'Horus Heresy',
+      'author': 'Dan Abnett',
+      'url': 'www.warhammer.com',
+      'likes': 5000,
+      'id': '6385cccbed1b7fbf96862999'
+    },
+    {
+      'title': 'Dead Rites',
+      'author': 'Jim Butcher',
+      'url': 'www.dresdenfiles.com',
+      'likes': 5000,
+      'id': '6385d08cb373f7f178bff582'
+    },
+    {
+      'title': 'Game of Thrones',
+      'author': 'GRR Martin',
+      'url': 'www.asongoficeandfire.com',
+      'likes': 30000,
+      'id': '6385d2e2cfab45040e8e19b7'
+    }
+  ]
+
+  // tests for MULTIPLE objects with maximum like
+  test('list of all blogs with maximum likes', () => {
+    const result = listHelper.favoriteBlog(list)
+    expect(result).toEqual([{
+      'title': 'The Hobbit',
+      'author': 'JRR Tolkien',
+      'likes': 30000,
+    },
+    {
+      'title': 'Game of Thrones',
+      'author': 'GRR Martin',
+      'likes': 30000,
+    }])
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,28 @@ const totalLikes = (blogs) => {
   }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const likes = blogs.map(blog => blog.likes)
+  const maxLikes = Math.max(...likes)
+  
+  const index = []
+  for(let i=0; i < likes.length; i++) {
+    (likes[i] === maxLikes) && index.push(i)
+  }
+
+  console.log(index)
+
+  return index.map(i => {
+    return {
+      title: blogs[i].title,
+      author: blogs[i].author,
+      likes: blogs[i].likes
+    }
+  })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
